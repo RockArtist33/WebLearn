1e1ed8e0c6b299d6aa7fda273e96f6c61f09fe74
RockArtist33 <jack-ogrady@outlook.com>
2023-12-03 23:42:25 +0000

    Made prototype course page

--- WebLearn/Controllers/CoursesController.cs
+++ WebLearn/Controllers/CoursesController.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using WebLearn.Data;
 using WebLearn.Models;
 
 namespace WebLearn.Controllers
 {
+    public class ViewModelCA
+    {
+        public IEnumerable<Courses>? Courses { get; set; }
+        public IEnumerable<Assignments>? Assignments { get; set; }
+    
+    }
     public class CoursesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -21,18 +29,26 @@ namespace WebLearn.Controllers
 
         public async Task<IActionResult> Index(int id)
         {   
+
             if (id == null || _context.Courses == null)
             {
                 return NotFound();
             }
 
-            var CourseOpen = await _context.Courses.FirstOrDefaultAsync(m => m.course_Id == id);
-            if (CourseOpen == null)
+            var CourseOpen = _context.Courses.Where(m => m.course_Id == id);
+            var AssignmentsOpen = _context.Assignments.Where(m => m.CourseId == id);
+            
+            var CompositeModel = new ViewModelCA();
+            CompositeModel.Courses = CourseOpen;
+            CompositeModel.Assignments = AssignmentsOpen;
+
+
+            if (CompositeModel.Courses == null || CompositeModel.Assignments == null)
             {
                 return NotFound();
             }
 
-            return View(CourseOpen);
+            return View(CompositeModel);
         }
     }
 }
