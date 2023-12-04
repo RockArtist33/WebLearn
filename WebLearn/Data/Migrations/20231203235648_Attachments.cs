dedcc2a42b62889eb60fff6c1de45bccec60c95d
RockArtist33 <jack-ogrady@outlook.com>
2023-12-04 00:43:39 +0000

    partially done attachments

---
+++ WebLearn/Data/Migrations/20231203235648_Attachments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebLearn.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Attachments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AssignmentId",
+                table: "Attachments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignmentId",
+                table: "Attachments");
+        }
+    }
+}
